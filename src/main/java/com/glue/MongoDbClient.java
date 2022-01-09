60c7858d5c6ff3be654104ad7d325e1ef5f4b231
Massih <massih.z@gmail.com>
2022-01-09 12:13:37 +0100

    Add testContainer for Mongo

--- src/main/java/com/glue/MongoDbClient.java
+++ src/main/java/com/glue/MongoDbClient.java
@@ -3,23 +3,25 @@ package com.glue;
 import com.mongodb.client.MongoClient;
 import dev.morphia.Datastore;
 import dev.morphia.Morphia;
+import io.micronaut.context.annotation.Context;
 import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
 
 
-@Singleton
+@Context
 public class MongoDbClient {
     private final MongoClient client;
+    private final Datastore datastore;
 
     @Inject
     public MongoDbClient(MongoClient client) {
         this.client = client;
+        datastore = Morphia.createDatastore(client, "starforks");
+        datastore.getMapper().mapPackage("com.glue");
+        datastore.ensureIndexes();
+
     }
 
     public Datastore getDataStore() {
-        Datastore ds = Morphia.createDatastore(client, "starforks");
-        ds.getMapper().mapPackage("com.glue");
-        ds.ensureIndexes();
-        return ds;
+        return datastore;
     }
 }
--- src/test/java/com/glue/StarforksTest.java
+++ src/test/java/com/glue/StarforksTest.java
@@ -3,19 +3,29 @@ package com.glue;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 
+@Testcontainers
 @MicronautTest
 class StarforksTest {
 
     @Inject
     EmbeddedApplication<?> application;
 
+    @Container
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:5.0.5"))
+            .withEnv("MONGO_INITDB_ROOT_USERNAME", "admin")
+            .withEnv("MONGO_INITDB_ROOT_PASSWORD", "admin").withExposedPorts(27017);
+
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
     }
-
 }
